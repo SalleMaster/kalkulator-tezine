a0eb9bd7137bbf5c1062071e4718b2186b465fcc
Sasa Radovanovic <salle90ar@gmail.com>
2019-07-16 00:43:33 +0200

    Initial setup, and menu finished

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+// Menu
+const menuButtons = document.querySelectorAll('.menu button');
+
+function handleMenu() {
+  menuButtons.forEach(button => button.classList.remove('active'));
+  this.classList.add('active');
+}
+
+menuButtons.forEach(button => button.addEventListener('click', handleMenu));
